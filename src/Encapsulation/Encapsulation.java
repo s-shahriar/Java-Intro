a9efc11149af0b57924abbb252f2fc97e7a23adc
s-shahriar <ksnkkc@gmail.com>
2024-11-21 16:01:24 +0600

    Encapsulation

---
+++ src/Encapsulation/Encapsulation.java
@@ -0,0 +1,35 @@
+package Encapsulation;
+
+class Human {
+    private int age;
+    private String name;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+}
+
+public class Encapsulation {
+    public static void main(String a[]){
+        Human obj = new Human();
+        obj.setAge(15);
+        obj.setName("Jack");
+
+        int age = obj.getAge();
+        String name = obj.getName();
+
+        System.out.println(name+ " "+ age);
+    }
+}
