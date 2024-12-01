326aa8196a32dab8fcbed688f4352dcf13b1a111
Syed Shahriar <50046492+s-shahriar@users.noreply.github.com>
2024-12-01 22:46:36 +0600

    Update JavaException.java

--- src/JavaException/JavaException.java
+++ src/JavaException/JavaException.java
@@ -2,6 +2,16 @@ package JavaException;
 
 public class JavaException {
 
+    // Method that throws an exception if division by zero occurs
+    // throws is used to handle the errpr in the paremt class (here Main class)
+    public static int divide(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            // Explicitly throwing an exception when division by zero occurs
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
     public static void main(String[] args) {
         try {
             // Simulating a situation where we explicitly throw an exception
@@ -20,14 +30,5 @@ public class JavaException {
             // Ensuring resource cleanup or logging, no matter what
             System.out.println("Finally: Cleanup resources or perform logging.");
         }
-    }
-
-    // Method that throws an exception if division by zero occurs
-    public static int divide(int a, int b) throws ArithmeticException {
-        if (b == 0) {
-            // Explicitly throwing an exception when division by zero occurs
-            throw new ArithmeticException("Cannot divide by zero");
-        }
-        return a / b;
     }
 }
